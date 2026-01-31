aa549afbfbd05b00fd41aad974b8c5a2f0ecb69e
Imam NurHadi <adigaming015@gmail.com>
2026-01-31 19:36:56 +0700

    Update: Change hash method

--- src/app/api/admin-login/route.js
+++ src/app/api/admin-login/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { getDb } from "@/lib/mongodb";
 import { createAdminCookie, COOKIE_NAME } from "@/lib/admin-auth";
 
